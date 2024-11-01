b38d14ba6bea9c7a2c4c72a8744aaad59215b355
Tim Wiechmann <Tim.Wiechmann@web.de>
2024-11-01 17:14:45 +0100

    Refactor LibrariesScreen to use AppTheme and new preview annotation

--- app/src/main/java/dr/achim/code_scanner/presentation/screens/libraries/LibrariesScreen.kt
+++ app/src/main/java/dr/achim/code_scanner/presentation/screens/libraries/LibrariesScreen.kt
@@ -3,44 +3,49 @@ package dr.achim.code_scanner.presentation.screens.libraries
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
 import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
-import dr.achim.code_scanner.presentation.components.DefaultAppBar
+import dr.achim.code_scanner.common.DefaultPreview
+import dr.achim.code_scanner.common.DynamicPreview
+import dr.achim.code_scanner.presentation.components.appbar.DefaultAppBar
 import dr.achim.code_scanner.presentation.navigation.NavigateUp
 import dr.achim.code_scanner.presentation.screens.Screen
 import dr.achim.code_scanner.presentation.theme.AppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LibrariesScreen(currentScreen: Screen, navigateUp: NavigateUp?) {
-    Scaffold(topBar = {
-        DefaultAppBar(
-            title = currentScreen.title,
-            canNavigateBack = navigateUp != null,
-            navigateUp = { navigateUp?.invoke() })
-    }) { innerPadding ->
+    Scaffold(
+        topBar = {
+            DefaultAppBar(
+                title = currentScreen.title,
+                navigateUp = navigateUp
+            )
+        },
+    ) { innerPadding ->
         Box(modifier = Modifier.padding(innerPadding)) {
             LibrariesContainer(
                 modifier = Modifier.fillMaxSize(),
                 colors = LibraryDefaults.libraryColors(
-                    backgroundColor = MaterialTheme.colorScheme.background,
-                    contentColor = contentColorFor(backgroundColor = MaterialTheme.colorScheme.background),
-                    dialogConfirmButtonColor = MaterialTheme.colorScheme.primary
+                    backgroundColor = AppTheme.colorScheme.background,
+                    contentColor = contentColorFor(backgroundColor = AppTheme.colorScheme.background),
+                    dialogConfirmButtonColor = AppTheme.colorScheme.primary
                 )
             )
         }
     }
 }
 
-@Preview
+@DynamicPreview
+@DefaultPreview
 @Composable
 fun LibrariesScreenPreview() {
     AppTheme {
-        LibrariesScreen(currentScreen = Screen.Libraries, {})
+        LibrariesScreen(currentScreen = Screen.Libraries) {}
     }
 }
