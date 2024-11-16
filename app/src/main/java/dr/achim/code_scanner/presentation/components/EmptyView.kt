98f7919926de951ea3c3826f2a9cce6bd7b601ea
Tim Wiechmann <Tim.Wiechmann@web.de>
2024-11-16 10:00:49 +0100

    Add empty view padding

--- app/src/main/java/dr/achim/code_scanner/presentation/components/EmptyView.kt
+++ app/src/main/java/dr/achim/code_scanner/presentation/components/EmptyView.kt
@@ -2,9 +2,11 @@ package dr.achim.code_scanner.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material3.Icon
@@ -20,13 +22,17 @@ import dr.achim.code_scanner.presentation.theme.AppTheme
 
 @Composable
 fun EmptyView(
-    modifier: Modifier = Modifier,
     title: String,
     description: String,
-    icon: ImageVector
+    icon: ImageVector,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(AppTheme.spacing.l)
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .then(modifier)
+            .padding(contentPadding),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(
             AppTheme.spacing.m,
